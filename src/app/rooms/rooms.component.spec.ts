9967a459efe2e2d20a8be8daead6943cd0c495cf
hasiq <jhasikowski@gmail.com>
2023-12-08 16:48:07 +0100

    added more tests

--- src/app/rooms/rooms.component.spec.ts
+++ src/app/rooms/rooms.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RoomsComponent } from './rooms.component';
+import { RoomsService } from './services/rooms.service';
+import { ConfigService } from 'src/services/config.service';
+import { HttpClientModule } from '@angular/common/http';
+import { APP_SERVICE_CONFIG } from '../AppConfig/appconfig.service';
+import { RouteConfigToken } from 'src/services/routeConfig.service';
 
 describe('RoomsComponent', () => {
   let component: RoomsComponent;
@@ -8,10 +13,21 @@ describe('RoomsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RoomsComponent]
-    })
-    .compileComponents();
-    
+      imports: [RoomsComponent, HttpClientModule],
+      providers: [
+        RoomsService,
+        ConfigService,
+        {
+          provide: APP_SERVICE_CONFIG,
+          useValue: { apiEndpoint: 'http://localhost:3000' },
+        },
+        {
+          provide: RouteConfigToken,
+          useValue: { title: 'rooms' },
+        },
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(RoomsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
