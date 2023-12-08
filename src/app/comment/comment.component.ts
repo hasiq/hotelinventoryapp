9a8834fa6ee219166dc4c521159d753aba77e8e8
hasiq <jhasikowski@gmail.com>
2023-12-08 16:08:13 +0100

    added resolve guard

--- src/app/comment/comment.component.ts
+++ src/app/comment/comment.component.ts
@@ -1,20 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { CommentService } from './comment.service';
+import { ActivatedRoute } from '@angular/router';
+import { map, pluck } from 'rxjs';
 
 @Component({
   selector: 'hinv-comment',
   templateUrl: './comment.component.html',
-  styleUrls: ['./comment.component.scss']
+  styleUrls: ['./comment.component.scss'],
 })
 export class CommentComponent implements OnInit {
-
-  
   comments$ = this.commentService.getComments();
 
-  constructor(private commentService : CommentService) { }
+  comment$ = this.activatedRoute.data.pipe(pluck('comments'));
 
+  comments: Comments[] = [];
+
+  constructor(
+    private commentService: CommentService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.activatedRoute.data.subscribe((data) => {
+      this.comments = data['comments'];
+    });
   }
-
 }
